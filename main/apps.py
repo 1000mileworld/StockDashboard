9f79b7166c9ff0c2acc74a3393adf56e5b796a01
Joe <jliang7558@gmail.com>
2022-02-10 22:26:20 -0500

    no DB operations in apps.py

--- main/apps.py
+++ main/apps.py
@@ -5,7 +5,7 @@ class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
     
-    #custom startup code
+    # custom startup code
     def ready(self) -> None:
         #return super().ready()
         from listItems.models import SearchField
@@ -16,20 +16,20 @@ class MainConfig(AppConfig):
         
         symbols = ['FB','AAPL','AMZN','NFLX','GOOGL']
 
-        Stock.objects.all().delete()
-        for i in range(len(symbols)):
-            tickers,companies = loadSP500()
-            convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
-            low, high, open, close = getYF_data(convertedTicker)
-            s = Stock(
-                ticker = convertedTicker,
-                company = convertedCompany,
-                open = open,
-                close = close,
-                low_52wk = low,
-                high_52wk = high
-            )
-            s.save()
+        # Stock.objects.all().delete()
+        # for i in range(len(symbols)):
+        #     tickers,companies = loadSP500()
+        #     convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
+        #     low, high, open, close = getYF_data(convertedTicker)
+        #     s = Stock(
+        #         ticker = convertedTicker,
+        #         company = convertedCompany,
+        #         open = open,
+        #         close = close,
+        #         low_52wk = low,
+        #         high_52wk = high
+        #     )
+        #     s.save()
 
-        SearchField.objects.all().delete()
-        SearchField(count=len(symbols)).save()
+        # SearchField.objects.all().delete()
+        # SearchField(count=len(symbols)).save()
