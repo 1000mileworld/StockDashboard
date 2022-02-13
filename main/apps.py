de7db8904386c7d460e8611ad7f30de4afadd03b
Joe <jliang7558@gmail.com>
2022-02-12 19:54:44 -0500

    no default tickers at startup

--- main/apps.py
+++ main/apps.py
@@ -17,19 +17,19 @@ class MainConfig(AppConfig):
         symbols = ['FB','AAPL','AMZN','NFLX','GOOGL']
 
         Stock.objects.all().delete()
-        for i in range(len(symbols)):
-            tickers,companies = loadSP500()
-            convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
-            low, high, open, close = getYF_data(convertedTicker)
-            s = Stock(
-                ticker = convertedTicker,
-                company = convertedCompany,
-                open = open,
-                close = close,
-                low_52wk = low,
-                high_52wk = high
-            )
-            s.save()
+        # for i in range(len(symbols)):
+        #     tickers,companies = loadSP500()
+        #     convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
+        #     low, high, open, close = getYF_data(convertedTicker)
+        #     s = Stock(
+        #         ticker = convertedTicker,
+        #         company = convertedCompany,
+        #         open = open,
+        #         close = close,
+        #         low_52wk = low,
+        #         high_52wk = high
+        #     )
+        #     s.save()
 
         SearchField.objects.all().delete()
         SearchField(count=len(symbols)).save()
