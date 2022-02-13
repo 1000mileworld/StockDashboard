ad222c5c62a0da5b94125cebf77dd60b6f80dfe1
Joe <jliang7558@gmail.com>
2022-02-13 10:52:08 -0500

    fix app ready code

--- main/apps.py
+++ main/apps.py
@@ -7,55 +7,37 @@ class MainConfig(AppConfig):
     
     # custom startup code
     def ready(self) -> None:
-        #return super().ready()
         from listItems.models import SearchField
         from stocks.models import Stock
         from stocks.func import saveSP500, loadSP500, validateInput, getYF_data
-        
+        from collections import namedtuple
+
         saveSP500()
         tickers,companies = loadSP500()
         
         symbols = ['FB','AAPL','AMZN','NFLX','GOOGL']
 
+        TickerInfo = namedtuple("PriceInfo","ticker company low high open close")
+        info_arr = set()
+        for i in range(len(symbols)):
+            convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
+            low, high, open, close = getYF_data(convertedTicker)
+            info_arr.add(TickerInfo(convertedTicker, convertedCompany, low, high, open, close))
+
+        info_arr = list(info_arr)
+
         Stock.objects.all().delete()
-        # for i in range(len(symbols)):
-        #     convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
-        #     low, high, open, close = getYF_data(convertedTicker)
-        #     s = Stock(
-        #         ticker = convertedTicker,
-        #         company = convertedCompany,
-        #         open = open,
-        #         close = close,
-        #         low_52wk = low,
-        #         high_52wk = high
-        #     )
-        #     s.save()
-
-        ticker = 'FB'
-        low, high, open, close = getYF_data(ticker)
-        s = Stock(
-            ticker = ticker,
-            company = 'Meta Platforms',
-            open = open,
-            close = close,
-            low_52wk = low,
-            high_52wk = high
-        )
-        s.save() #save apparently is being called twice
-
-        #Stock.objects.all().delete() #this works
-
-        ticker = 'AAPL'
-        low, high, open, close = getYF_data(ticker)
-        s = Stock(
-            ticker = ticker,
-            company = 'Apple',
-            open = open,
-            close = close,
-            low_52wk = low,
-            high_52wk = high
-        )
-        s.save()
+        for i in range(len(info_arr)):
+            ticker_info = info_arr[i]
+            s = Stock(
+                ticker = ticker_info.ticker,
+                company = ticker_info.company,
+                open = ticker_info.open,
+                close = ticker_info.close,
+                low_52wk = ticker_info.low,
+                high_52wk = ticker_info.high
+            )
+            s.save()
 
         SearchField.objects.all().delete()
-        SearchField(count=len(symbols)).save()
+        SearchField(count=len(info_arr)).save()
