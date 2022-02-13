d184bea7ecba641b8fc65d588b11be25cfd77c35
Joe <jliang7558@gmail.com>
2022-02-13 11:10:53 -0500

    barebones view

--- listItems/admin.py
+++ listItems/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Search, SearchField
+from .models import Search,SearchField
 # Register your models here.
 
-admin.site.register(Search)
 admin.site.register(SearchField)
--- main/views.py
+++ main/views.py
@@ -16,55 +16,56 @@ from stocks import alpaca
 stock_limit = 10
 # Create your views here.
 def index(request):
-    ticker,company = loadSP500() # load list of ticker symbols and company names
-    isValid = True
-    searchResults = Stock.objects.all()
-    curr_stocks = set(Stock.objects.values_list('ticker', flat=True)) # flat returned results are single values, rather than one-tuples
+    # ticker,company = loadSP500() # load list of ticker symbols and company names
+    # isValid = True
+    # searchResults = Stock.objects.all()
+    # curr_stocks = set(Stock.objects.values_list('ticker', flat=True)) # flat returned results are single values, rather than one-tuples
  
-    searchInfo = SearchField.objects.first()
-    searchInfo.count = Stock.objects.count()
+    # searchInfo = SearchField.objects.first()
+    # searchInfo.count = Stock.objects.count()
 
-    if request.method == 'POST':
-        input = request.POST['searchBar']
-        isValid,convertedTicker,convertedCompany = validateInput(input,ticker,company)
-        if isValid and searchInfo.count<stock_limit: # don't go over limit
-            searchInfo.validity = True
-            if convertedTicker not in curr_stocks: # no duplicate entered
-                searchInfo.is_duplicate = False
-                low, high, open, close = getYF_data(convertedTicker)
-                new_stock = Stock(
-                    ticker = convertedTicker,
-                    company = convertedCompany,
-                    open = open,
-                    close = close,
-                    low_52wk = low,
-                    high_52wk = high
-                )
-                new_stock.save()
+    # if request.method == 'POST':
+    #     input = request.POST['searchBar']
+    #     isValid,convertedTicker,convertedCompany = validateInput(input,ticker,company)
+    #     if isValid and searchInfo.count<stock_limit: # don't go over limit
+    #         searchInfo.validity = True
+    #         if convertedTicker not in curr_stocks: # no duplicate entered
+    #             searchInfo.is_duplicate = False
+    #             low, high, open, close = getYF_data(convertedTicker)
+    #             new_stock = Stock(
+    #                 ticker = convertedTicker,
+    #                 company = convertedCompany,
+    #                 open = open,
+    #                 close = close,
+    #                 low_52wk = low,
+    #                 high_52wk = high
+    #             )
+    #             new_stock.save()
                 
-                #searchInfo.count+=1
-            else:
-                searchInfo.is_duplicate = True
-        else:
-            searchInfo.validity = False # input not found
+    #             #searchInfo.count+=1
+    #         else:
+    #             searchInfo.is_duplicate = True
+    #     else:
+    #         searchInfo.validity = False # input not found
         
-        searchInfo.save() #need to keep this here because of redirect
-        return redirect('/') #homepage
+    #     searchInfo.save() #need to keep this here because of redirect
+    #     return redirect('/') #homepage
     
-    searchInfo.save()
-    res = [i +' - '+j for i, j in zip(ticker, company)] #'TICKER - Company'
-    stock_ids = [str(s.id) for s in searchResults]
+    # searchInfo.save()
+    # res = [i +' - '+j for i, j in zip(ticker, company)] #'TICKER - Company'
+    # stock_ids = [str(s.id) for s in searchResults]
 
-    market_status = alpaca.isOpen()
-    context = {
-        'to_add': searchResults,
-        'res': res,
-        'info': searchInfo,
-        'stock_ids': stock_ids,
-        'market_status': market_status,
-    }
+    # market_status = alpaca.isOpen()
+    # context = {
+    #     'to_add': searchResults,
+    #     'res': res,
+    #     'info': searchInfo,
+    #     'stock_ids': stock_ids,
+    #     'market_status': market_status,
+    # }
 
-    return render(request, 'main/base.html', context)
+    # return render(request, 'main/base.html', context)
+    return render(request, 'main/base.html')
 
 def delete(request, pk):
 
