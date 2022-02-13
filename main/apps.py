1c6ebe27df55a6378aba17c194d3fedfe06b86ad
Joe <jliang7558@gmail.com>
2022-02-13 11:28:54 -0500

    barebones model

--- main/apps.py
+++ main/apps.py
@@ -14,30 +14,37 @@ class MainConfig(AppConfig):
 
         saveSP500()
         tickers,companies = loadSP500()
-        
         symbols = ['FB','AAPL','AMZN','NFLX','GOOGL']
-
-        TickerInfo = namedtuple("PriceInfo","ticker company low high open close")
-        info_arr = set()
-        for i in range(len(symbols)):
-            convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
-            low, high, open, close = getYF_data(convertedTicker)
-            info_arr.add(TickerInfo(convertedTicker, convertedCompany, low, high, open, close))
-
-        info_arr = list(info_arr)
-
         Stock.objects.all().delete()
-        for i in range(len(info_arr)):
-            ticker_info = info_arr[i]
+
+        # TickerInfo = namedtuple("PriceInfo","ticker company low high open close")
+        # info_arr = set()
+        # for i in range(len(symbols)):
+        #     convertedTicker,convertedCompany = validateInput(symbols[i],tickers,companies)[1:3]
+        #     low, high, open, close = getYF_data(convertedTicker)
+        #     info_arr.add(TickerInfo(convertedTicker, convertedCompany, low, high, open, close))
+
+        # info_arr = list(info_arr)
+
+        # for i in range(len(info_arr)):
+        #     ticker_info = info_arr[i]
+        #     s = Stock(
+        #         ticker = ticker_info.ticker,
+        #         company = ticker_info.company,
+        #         open = ticker_info.open,
+        #         close = ticker_info.close,
+        #         low_52wk = ticker_info.low,
+        #         high_52wk = ticker_info.high
+        #     )
+        #     s.save()
+
+        
+        for i in range(len(symbols)):
             s = Stock(
-                ticker = ticker_info.ticker,
-                company = ticker_info.company,
-                open = ticker_info.open,
-                close = ticker_info.close,
-                low_52wk = ticker_info.low,
-                high_52wk = ticker_info.high
+                ticker = symbols[i],
+                company = "",
             )
             s.save()
 
         SearchField.objects.all().delete()
-        SearchField(count=len(info_arr)).save()
+        SearchField(count=len(symbols)).save()
